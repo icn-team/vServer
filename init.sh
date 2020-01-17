f81e8f24654892a294ee944b216af00df989e3bc
acompagn <acompagn@cisco.com>
2020-01-17 09:59:06 +0100

    fixed error in the dpdk section

--- init.sh
+++ init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Configure VPP"
 if [ -n "$DPDK" ]
+   BLOCK=""
 then
     BLOCK="dpdk { $DPDK }"
 fi
@@ -10,28 +11,22 @@ then
     NUM_BUFFER=16384
 fi
 
-#eval sed "s/NUM_BUFFER/\"$(echo $NUM_BUFFER)\"/g" startup.conf
-
 if [ ! -n "$PIT_SIZE" ]
 then
     PIT_SIZE=8192
 fi
-
-# eval sed "s/PIT_SIZE/\"$(echo $PIT_SIZE)\"/g" startup_temp.conf > startup_temp.conf
 
 if [ ! -n "$CS_SIZE" ]
 then
     CS_SIZE=4096
 fi
 
-# eval sed "s/CS_SIZE/\"$(echo $CS_SIZE)\"/g" startup_temp.conf > startup_temp.conf
-
 if [ ! -n "$CS_RESERVED_APP" ]
 then
     CS_RESERVED_APP=20
 fi
 
-eval sed -e "s/DPDK_DEVICES/\"$(echo $BLOCK)\"/g" -e "s/NUM_BUFFER/\"$(echo $NUM_BUFFER)\"/g" -e "s/PIT_SIZE/\"$(echo $PIT_SIZE)\"/g" -e "s/CS_SIZE/\"$(echo $CS_SIZE)\"/g" -e "s/CS_RESERVED_APP/\"$(echo $CS_RESERVED_APP)\"/g" /tmp/startup_template.conf > /etc/vpp/startup.conf
+eval sed -e "s/DPDK/\"$(echo $BLOCK)\"/g" -e "s/NUM_BUFFER/\"$(echo $NUM_BUFFER)\"/g" -e "s/PIT_SIZE/\"$(echo $PIT_SIZE)\"/g" -e "s/CS_SIZE/\"$(echo $CS_SIZE)\"/g" -e "s/CS_RESERVED_APP/\"$(echo $CS_RESERVED_APP)\"/g" /tmp/startup_template.conf > /etc/vpp/startup.conf
 
 echo "Run vpp"
 /usr/bin/vpp -c /etc/vpp/startup.conf
